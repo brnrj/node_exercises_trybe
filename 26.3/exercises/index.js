36054d32c5af1752b7fe84777ed462386c5ed911
Bruno Martins <bruno.martins217@gmail.com>
2021-05-19 01:27:28 -0300

    Fix

--- 26.3/exercises/index.js
+++ 26.3/exercises/index.js
@@ -1,16 +1,15 @@
 const { writeFileSync } = require('fs');
 
-function callNumber(number){
-  if(typeof number !== 'number') return 'o valor deve ser um numero'
-  if(number > 0) return 'positivo'
-  if(number < 0) return 'negativo'
-  return 'neutro'
+function callNumber(number) {
+  if (typeof number !== 'number') return 'o valor deve ser um numero';
+  if (number > 0) return 'positivo';
+  if (number < 0) return 'negativo';
+  return 'neutro';
 }
 
-function writeFile(nomeArquivo, content){
-  writeFileSync(nomeArquivo, content)
-  return 'ok'
+function writeFile(nomeArquivo, content) {
+  writeFileSync(`${__dirname}/${nomeArquivo}`, content);
+  return 'ok';
 }
-
 
 module.exports = { callNumber, writeFile };
